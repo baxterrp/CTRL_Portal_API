c891585937eba7f2b0fdad9a523f5793996f64f3
Robert Baxter <baxterrp@gmail.com>
2021-01-08 12:10:56 -0500

    added UsersController for future tasks

---
+++ CTRL.Portal.API/Controllers/UsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CTRL.Portal.API.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+    }
+}
