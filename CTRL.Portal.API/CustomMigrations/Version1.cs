fe713a149c5eada9f016119234aa75ab600f597b
Robert Baxter <baxterrp@gmail.com>
2021-01-15 15:56:32 -0500

    added migrations

---
+++ CTRL.Portal.API/CustomMigrations/Version1.cs
@@ -0,0 +1,31 @@
+﻿using FluentMigrator;
+using FluentMigrator.SqlServer;
+
+namespace CTRL.Portal.API.CustomMigrations
+{
+    [Migration(01152021)]
+    public class Version1 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table("UserAccounts");
+            Delete.Table("accounts");
+        }
+
+        public override void Up()
+        {
+            Create.Table("Accounts")
+                .WithColumn("Id").AsString().NotNullable().PrimaryKey()
+                .WithColumn("Name").AsString().NotNullable();
+
+            Create.Table("UserAccounts")
+                .WithColumn("Id").AsInt32().PrimaryKey().Identity(1, 1)
+                .WithColumn("AccountId").AsString().NotNullable().ForeignKey()
+                .WithColumn("UserName").AsString().NotNullable();
+
+            Create.ForeignKey()
+                .FromTable("UserAccounts").ForeignColumn("AccountId")
+                .ToTable("Accounts").PrimaryColumn("Id");
+        }
+    }
+}
--- CTRL.Portal.API/Startup.cs
+++ CTRL.Portal.API/Startup.cs
@@ -1,9 +1,11 @@
+
 using CTRL.Portal.API.Configuration;
 using CTRL.Portal.API.EntityContexts;
 using CTRL.Portal.API.Middleware;
 using CTRL.Portal.API.Services;
 using CTRL.Portal.Data.Configuration;
 using CTRL.Portal.Data.Repositories;
+using FluentMigrator.Runner;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -13,10 +15,30 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Linq;
 using System.Text;
 
 namespace CTRL.Portal.API
 {
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddFluentMigrator(this IServiceCollection services, string connectionString)
+        {
+            var migrationAssemblies = AppDomain.CurrentDomain.GetAssemblies()
+                .SelectMany(assembly => assembly.GetTypes())
+                .Where(t => t.IsClass && t.Namespace == "CTRL.Portal.API.CustomMigrations")
+                .Select(x => x.Assembly).ToArray();
+
+            services.AddFluentMigratorCore()
+                .ConfigureRunner(runner => runner.AddSqlServer()
+                    .WithGlobalConnectionString(connectionString)
+                    .ScanIn(migrationAssemblies).For.Migrations());
+
+            return services;
+        }
+    }
+
     public class Startup
     {
         public Startup(IConfiguration configuration)
@@ -25,6 +47,8 @@ namespace CTRL.Portal.API
         }
 
         public IConfiguration Configuration { get; }
+
+
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
@@ -42,6 +66,8 @@ namespace CTRL.Portal.API
                 ConnectionString = connectionString
             };
             services.AddSingleton(dbConfig);
+
+            services.AddFluentMigrator(connectionString);
 
             services.AddDbContext<ApplicationDbContext>(options =>
             {
@@ -83,10 +109,11 @@ namespace CTRL.Portal.API
                     .AllowAnyOrigin()
                     .AllowAnyHeader();
             }));
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, IMigrationRunner runner)
         {
             if (env.IsDevelopment())
             {
@@ -102,13 +129,15 @@ namespace CTRL.Portal.API
             app.UseCors("StandardPolicy");
 
             app.UseAuthorization();
-            
+
             app.UseMiddleware(typeof(ApiMiddleware));
 
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
             });
+
+            runner.MigrateUp();
         }
     }
 }
