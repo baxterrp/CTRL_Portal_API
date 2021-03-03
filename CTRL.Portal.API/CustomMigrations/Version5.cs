6d65fe92af942491e9391bf673d996c7055d8fa4
Robert Baxter <baxterrp@gmail.com>
2021-03-03 15:26:13 -0500

    added module, using constants for table names (#16)

---
+++ CTRL.Portal.API/CustomMigrations/Version5.cs
@@ -0,0 +1,29 @@
+﻿using CTRL.Portal.API.APIConstants;
+using FluentMigrator;
+using System;
+
+namespace CTRL.Portal.API.CustomMigrations
+{
+    [Migration(5)]
+    public class Version5 : Migration
+    {
+        public override void Down()
+        {
+            Delete.Table(ApiNames.ModulesTable);
+        }
+
+        public override void Up()
+        {
+            Create.Table(ApiNames.ModulesTable)
+                .WithColumn("Id").AsString().NotNullable().PrimaryKey()
+                .WithColumn("Name").AsString().NotNullable();
+
+            Insert.IntoTable("Modules").Row(
+                new
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = ApiNames.InventoryModule
+                });
+        }
+    }
+}
