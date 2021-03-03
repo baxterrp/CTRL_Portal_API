36183e5dd3e0755c79b8c46f22bb60643791de65
Robert Baxter <baxterrp@gmail.com>
2021-03-03 15:25:16 -0500

    added module, using constants for table names

--- CTRL.Portal.API/CustomMigrations/Version4.cs
+++ CTRL.Portal.API/CustomMigrations/Version4.cs
@@ -1,5 +1,5 @@
-﻿using FluentMigrator;
-using FluentMigrator.SqlServer;
+﻿using CTRL.Portal.API.APIConstants;
+using FluentMigrator;
 
 namespace CTRL.Portal.API.CustomMigrations
 {
@@ -8,12 +8,12 @@ namespace CTRL.Portal.API.CustomMigrations
     {
         public override void Down()
         {
-            Delete.Table("Codes");
+            Delete.Table(ApiNames.CodesTable);
         }
 
         public override void Up()
         {
-            Create.Table("Codes")
+            Create.Table(ApiNames.CodesTable)
                 .WithColumn("Id").AsString().NotNullable().PrimaryKey()
                 .WithColumn("Email").AsString().NotNullable()
                 .WithColumn("Expiration").AsDateTime().NotNullable()
