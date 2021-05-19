1c2b4267d7025aa84e31cc7e3d4a82ba7fde7e84
Robert Baxter <baxterrp@gmail.com>
2021-05-19 16:38:46 -0400

    added modules to create sub (#30)

--- CTRL.Portal.Common/Contracts/SubscriptionContract.cs
+++ CTRL.Portal.Common/Contracts/SubscriptionContract.cs
@@ -1,8 +1,11 @@
-﻿namespace CTRL.Portal.Common.Contracts
+﻿using System.Collections.Generic;
+
+namespace CTRL.Portal.Common.Contracts
 {
     public class SubscriptionContract
     {
         public string BusinessEntityId { get; set; }
         public string Name { get; set; }
+        public List<string> ModuleIds { get; set; }
     }
 }
